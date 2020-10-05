70bdd4d5cb91a38389d75b0ee37150120ede673a
Paolo Ruggirello <pruggirello@soldo.com>
2020-10-05 18:13:15 +0200

    first commit

---
+++ src/main/java/com/example/registrocalcio/RegistroCalcioApplication.java
@@ -0,0 +1,13 @@
+package com.example.registrocalcio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroCalcioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroCalcioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/registrocalcio/RegistroCalcioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registrocalcio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroCalcioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
