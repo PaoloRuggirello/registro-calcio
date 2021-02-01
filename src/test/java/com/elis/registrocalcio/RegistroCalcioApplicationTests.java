52ddfea3620d7a58a2f43769c448e68f12226b45
paoloruggirello <ruggirello99@hotmail.it>
2021-02-01 23:02:10 +0100

    fixed tests

--- src/test/java/com/elis/registrocalcio/RegistroCalcioApplicationTests.java
+++ src/test/java/com/elis/registrocalcio/RegistroCalcioApplicationTests.java
@@ -2,12 +2,19 @@ package com.elis.registrocalcio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class RegistroCalcioApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void test(){
+		System.out.println("HERE");
+		Assert.isTrue(true);
 	}
 
 }
