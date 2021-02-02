09cca9be45d3df839696812afd64ba65ea389af9
paoloruggirello <ruggirello99@hotmail.it>
2021-02-02 08:56:25 +0100

    Added test to crypt password

--- src/test/java/com/elis/registrocalcio/RegistroCalcioApplicationTests.java
+++ src/test/java/com/elis/registrocalcio/RegistroCalcioApplicationTests.java
@@ -1,20 +1,25 @@
 package com.elis.registrocalcio;
 
+import com.elis.registrocalcio.handler.UserHandler;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 @SpringBootTest
 class RegistroCalcioApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	UserHandler userHandler;
 
 	@Test
-	public void test(){
-		System.out.println("HERE");
-		Assert.isTrue(true);
+	public void cryptPassword() throws InvalidKeySpecException, NoSuchAlgorithmException {
+		String password = "admin";
+		String passwordEncrypted = userHandler.passwordEncryption(password);
+		System.out.println(passwordEncrypted);
 	}
 
 }
